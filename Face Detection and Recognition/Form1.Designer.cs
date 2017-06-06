f936dc01ad7106fcfad8d42b8d9ed87cf74b37e7
Sarawoot Pattamapong <ponlovet@hotmail.com>
2017-06-06 12:13:06 +0700

    Add project resorce and code

---
+++ Face Detection and Recognition/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace CreateTrainingSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.filming = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loadImage = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.saveImage = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(164)))), ((int)(((byte)(154)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Exit);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1458, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ระบบยืนยันบุคคลด้วยใบหน้า";
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(49)))), ((int)(((byte)(110)))));
+            this.Exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Exit.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Exit.FlatAppearance.BorderSize = 0;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit.Location = new System.Drawing.Point(1325, 0);
+            this.Exit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(44, 45);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "X";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(232)))), ((int)(((byte)(210)))));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 81);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(232)))), ((int)(((byte)(210)))));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(1037, 81);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(287, 416);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // filming
+            // 
+            this.filming.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(97)))), ((int)(((byte)(157)))));
+            this.filming.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.filming.FlatAppearance.BorderSize = 0;
+            this.filming.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.filming.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.filming.Location = new System.Drawing.Point(70, 650);
+            this.filming.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.filming.Name = "filming";
+            this.filming.Size = new System.Drawing.Size(147, 46);
+            this.filming.TabIndex = 3;
+            this.filming.Text = "ถ่ายภาพ";
+            this.filming.UseVisualStyleBackColor = false;
+            this.filming.Click += new System.EventHandler(this.filming_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(277, 652);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(408, 44);
+            this.textBox1.TabIndex = 11;
+            // 
+            // loadImage
+            // 
+            this.loadImage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(97)))), ((int)(((byte)(157)))));
+            this.loadImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.loadImage.FlatAppearance.BorderSize = 0;
+            this.loadImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadImage.Location = new System.Drawing.Point(698, 652);
+            this.loadImage.Name = "loadImage";
+            this.loadImage.Size = new System.Drawing.Size(147, 46);
+            this.loadImage.TabIndex = 12;
+            this.loadImage.Text = "โหลดภาพ";
+            this.loadImage.UseVisualStyleBackColor = false;
+            this.loadImage.Click += new System.EventHandler(this.loadImage_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(1150, 514);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ไม่พบข้อมูล";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox2.Location = new System.Drawing.Point(1030, 589);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(303, 32);
+            this.textBox2.TabIndex = 6;
+            // 
+            // saveImage
+            // 
+            this.saveImage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(97)))), ((int)(((byte)(157)))));
+            this.saveImage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveImage.FlatAppearance.BorderSize = 0;
+            this.saveImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveImage.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.saveImage.Location = new System.Drawing.Point(1116, 640);
+            this.saveImage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.saveImage.Name = "saveImage";
+            this.saveImage.Size = new System.Drawing.Size(140, 46);
+            this.saveImage.TabIndex = 10;
+            this.saveImage.Text = "เพิ่มข้อมูล";
+            this.saveImage.UseVisualStyleBackColor = false;
+            this.saveImage.Click += new System.EventHandler(this.saveImage_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(1025, 558);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 26);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "ชื่อ-สกุล";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(219)))), ((int)(((byte)(219)))));
+            this.ClientSize = new System.Drawing.Size(1368, 723);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.saveImage);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loadImage);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.filming);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button filming;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button loadImage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button saveImage;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Face Detection and Recognition/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+
+namespace CreateTrainingSet
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, Byte> currentFrame, normalizedImage;
+        Capture grabber;
+        HaarCascade face = new HaarCascade("haarcascade_frontalface_default.xml");
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5d, 0.5d);
+        Image<Gray, byte> result, TrainedFace = null;
+        Image<Gray, byte> gray = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
+        bool buttonCheck = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Load of previus trainned faces and labels for each image
+            try
+            {
+                //Load of previus trainned faces and labels for each image
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                string LoadFaces;
+
+                for (int tf = 1; tf < NumLabels + 1; tf++)
+                {
+                    LoadFaces = "face" + tf + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/TrainedFaces/" + LoadFaces));
+                    labels.Add(Labels[tf]);
+                }
+
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.ToString());
+                MessageBox.Show("ฐานข้อมูลว่างเปล่า กรุณาเพิ่มข้อมูล", "โหลดฐานข้อมูล", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        
+
+        private void loadImage_Click_1(object sender, EventArgs e)
+        {   
+            if(buttonCheck == true)
+            {
+                timer1.Stop();
+                filming.Text = "ถ่ายภาพ";
+                grabber.Dispose();
+                buttonCheck = false;
+            }
+
+            NamePersons.Add("");
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    pictureBox1.Load(openFileDialog1.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+                catch (System.ArgumentException)
+                {
+                    Form2 frm = new Form2();
+                    frm.Show();
+                }
+            }
+            
+                // Converting the master image to a bitmap
+                Bitmap masterImage = (Bitmap)pictureBox1.Image;
+
+            try
+            {
+                // Normalizing it to grayscale
+                normalizedImage = new Image<Bgr, Byte>(masterImage);
+            }
+            catch (System.NullReferenceException)
+            {
+                return;
+            }
+
+                MCvAvgComp[][] facesDetected = normalizedImage.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(Width / 8, Height / 8));
+                //Action for each element detected
+                foreach (MCvAvgComp f in facesDetected[0])
+                {
+                    t = t + 1;
+                    result = normalizedImage.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                    //draw the face detected in the 0th (gray) channel with blue color
+                    normalizedImage.Draw(f.rect, new Bgr(Color.Red), 2);
+
+                    if (trainingImages.ToArray().Length != 0)
+                    {
+                        //TermCriteria for face recognition with numbers of trained images like maxIteration
+                        MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+
+                        //Eigen face recognizer
+                        EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 10000, ref termCrit);
+
+                        name = recognizer.Recognize(result).Label;
+
+                        //Draw the label for each face detected and recognized
+                        label3.Text = names;
+
+                    }
+
+                    NamePersons[t - 1] = name;
+                    NamePersons.Add("");
+                }
+                t = 0;
+
+                //Names concatenation of persons recognized
+                for (int nnn = 0; nnn < facesDetected[0].Length; nnn++)
+                {
+                    names = names + NamePersons[nnn] + ", ";
+                }
+                //Show the faces procesed and recognized
+                pictureBox1.Image = normalizedImage.ToBitmap();
+                buttonCheck = true;
+
+                names = "";
+                label3.Text = name;
+                //Clear the list(vector) of names
+                NamePersons.Clear();
+                pictureBox2.InitialImage = null;
+
+                if (name != null)
+                {
+                try
+                {
+                    pictureBox2.Load("C:/faceRecognized/" + name + ".jpg");
+                    pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+                catch (System.IO.FileNotFoundException) { label3.Text = "ไม่พบรูปในฐานข้อมูล"; }
+            }
+            
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveImage_Click(object sender, EventArgs e)
+        {
+                
+                //Trained face counter
+                ContTrain = ContTrain + 1;
+
+                Bitmap masterImage2 = (Bitmap)pictureBox1.Image;
+                // Normalizing it to grayscale
+                Image<Gray, Byte> gray = new Image<Gray, Byte>(masterImage2);
+
+            if (buttonCheck == false)
+            {
+                //Get a gray frame from capture device
+                gray = grabber.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            }
+                //Face Detector
+                MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+            //Action for each element detected
+            foreach (MCvAvgComp f in facesDetected[0])
+            {
+
+
+                //resize face detected image for force to compare the same size with the 
+                //test image with cubic interpolation type method
+                TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBox2.Text);
+
+
+                //Show face added in gray scale
+                pictureBox2.Image = TrainedFace.ToBitmap();
+
+                //Write the number of triained faces in a file text for further load
+                File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImages.ToArray().Length.ToString() + "%");
+
+                //Write the labels of triained faces in a file text for further load
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/TrainedFaces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labels.ToArray()[i - 1] + "%");
+                }
+                MessageBox.Show("บันทึกข้อมูลคุณ " + textBox2.Text + " เรียบร้อย", "Training OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+                
+        }
+
+        private void filming_Click(object sender, EventArgs e)
+        {
+            if (buttonCheck == false)
+            {
+                //Initialize the capture device
+                grabber = new Capture();
+                grabber.QueryFrame();
+                //Initialize the FrameGraber event
+                timer1.Start();
+                filming.Text = "หยุด";
+                buttonCheck = true;
+            }
+            else
+            {
+                timer1.Stop();
+                filming.Text = "ถ่ายภาพ";
+                grabber.Dispose();
+                //currentFrame = null;
+                buttonCheck = false;
+                
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NamePersons.Add("");
+
+            //Get the current frame form capture device
+            currentFrame = grabber.QueryFrame().Resize(640, 360, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            //Convert it to Grayscale
+            gray = currentFrame.Convert<Gray, Byte>();
+            //Face Detector
+            MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(Width / 8, Height / 8));
+
+            //Action for each element detected
+            foreach (MCvAvgComp f in facesDetected[0])
+            {
+                t = t + 1;
+                result = currentFrame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                //draw the face detected in the 0th (gray) channel with blue color
+                currentFrame.Draw(f.rect, new Bgr(Color.Red), 2);
+
+
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    //TermCriteria for face recognition with numbers of trained images like maxIteration
+                    MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+
+                    //Eigen face recognizer
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 3000, ref termCrit);
+
+                    try
+                    {
+                        name = recognizer.Recognize(result).Label;
+                    }
+                    catch (System.NullReferenceException){}
+                    
+
+                    //Draw the label for each face detected and recognized
+                    //currentFrame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.LightGreen));
+
+                }
+
+                NamePersons[t - 1] = name;
+                NamePersons.Add("");
+
+
+            }
+            t = 0;
+
+            //Names concatenation of persons recognized
+            for (int nnn = 0; nnn < facesDetected[0].Length; nnn++)
+            {
+                names = names + NamePersons[nnn];
+            }
+            //Show the faces procesed and recognized
+            pictureBox1.Image = currentFrame.ToBitmap();
+            pictureBox1.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            names = "";
+            label3.Text = name;
+            //Clear the list(vector) of names
+            NamePersons.Clear();
+            pictureBox2.InitialImage = null;
+
+            if (name != null)
+            {
+                try
+                {
+                    pictureBox2.Load("C:/faceRecognized/" + name + ".jpg");
+                    pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+                catch (System.IO.FileNotFoundException) { label3.Text = "ไม่พบรูปในฐานข้อมูล"; }
+            }
+        }
+    }
+}
